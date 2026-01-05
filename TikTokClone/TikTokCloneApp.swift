8899a2945f2ccbb4c849916b3cec5ff52f28f3b7
giogocadze <giogocadze5@gmail.com>
2026-01-05 15:00:59 +0400

    Initial commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by giorgi gotsadze on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TikTokCloneTests/TikTokCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneTests.swift
+//  TikTokCloneTests
+//
+//  Created by giorgi gotsadze on 1/5/26.
+//
+
+import Testing
+@testable import TikTokClone
+
+struct TikTokCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
